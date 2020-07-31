21d8c9442f434a11bf7ba2f1bfc324fc3c904a54
coldsummerday <zhou2808972009@outlook.com>
2020-07-31 19:14:51 +0800

    update setup.py add DeformConv

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env python
 
 from distutils.core import setup, Extension
+import setuptools
 import os
 import platform
 
@@ -9,6 +10,12 @@ code from:
 https://pythonextensionpatterns.readthedocs.io/en/latest/compiler_flags.html
 """
 import sysconfig
+import torch
+
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension,CppExtension
+
+
+BASE_DIR  = os.path.split(os.path.realpath(__file__))[0]
 _DEBUG = False
 # Generally I write code so that if DEBUG is defined as 0 then all optimisations
 # are off and asserts are enabled. Typically run times of these builds are x2 to x10
@@ -27,13 +34,38 @@ else:
     extra_compile_args += ["-DNDEBUG", "-O3"]
 
 def make_cpp_ext(name:str,module:str,sources:list,include_dirs:list,library_dirs:list=[],**kwargs):
-    return Extension(
+    return CppExtension(
         name='{}.{}'.format(module, name),
         sources=[os.path.join(*module.split('.'), p) for p in sources],
-        include_dirs = [os.path.join(*module.split("."),p) for p in include_dirs],
+        include_dirs = [os.path.join(BASE_DIR,*module.split("."),p) for p in include_dirs],
         ##extra_link_args =sysconfig.get_config_vars('LDFLAGS'),
         **kwargs
     )
+
+def make_cuda_ext(name, module, sources=[], sources_cuda=[]):
+
+    define_macros = []
+    extra_compile_args = {'cxx': []}
+
+    if torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1':
+        define_macros += [('WITH_CUDA', None)]
+        extension = CUDAExtension
+        extra_compile_args['nvcc'] = [
+            '-D__CUDA_NO_HALF_OPERATORS__',
+            '-D__CUDA_NO_HALF_CONVERSIONS__',
+            '-D__CUDA_NO_HALF2_OPERATORS__',
+        ]
+        sources += sources_cuda
+    else:
+        print(f'Compiling {name} without CUDA')
+        extension = Extension
+        # raise EnvironmentError('CUDA is required to compile MMDetection!')
+
+    return extension(
+        name='{}.{}'.format(module, name),
+        sources=[os.path.join(*module.split('.'), p) for p in sources],
+        define_macros=define_macros,
+        extra_compile_args=extra_compile_args)
 
 if __name__ == '__main__':
     setup(
@@ -48,6 +80,25 @@ if __name__ == '__main__':
                 extra_compile_args=extra_compile_args,
                 language='c++11',
 
-            )
-            ]
+            ),
+            make_cuda_ext(
+                name='deform_conv_cuda',
+                module="texthub.ops.dcn",
+                sources=[],
+                sources_cuda=[
+                    'src/deform_conv_cuda.cpp',
+                    'src/deform_conv_cuda_kernel.cu'
+                ]),
+            make_cuda_ext(
+                name='deform_pool_cuda',
+                module="texthub.ops.dcn",
+                sources=[],
+                sources_cuda=[
+                     'src/deform_pool_cuda.cpp',
+                    'src/deform_pool_cuda_kernel.cu'
+                ])
+            ],
+        cmdclass={'build_ext': BuildExtension}
     )
+
+
